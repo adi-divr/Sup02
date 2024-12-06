5f806821c1bda8573fa84b18df852c3835d433e6
unknown <adityatl@datadivr.com>
2024-12-06 23:13:32 +0530

    admin- final commit

--- myapp/next.config.ts
+++ myapp/next.config.ts
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export', // Enables static export
-  basePath: '/<Sup02>', // Replace with your GitHub repository name
+  basePath: '/Sup02', // Replace with your GitHub repository name
   images: {
     unoptimized: true, // Required if you're using the Image component
   },
